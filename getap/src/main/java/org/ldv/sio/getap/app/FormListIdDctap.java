c5eee062b5b2bef17ef95e483255ed5dbfdd44fd
Florian Laforest <laforest.florian@gmail.com>
2012-06-19 21:39:11 +0200

    Les professeurs ont la possibilités de valider/refuser plusieurs demandes de validation à la fois

---
+++ getap/src/main/java/org/ldv/sio/getap/app/FormListIdDctap.java
@@ -0,0 +1,15 @@
+package org.ldv.sio.getap.app;
+
+public class FormListIdDctap {
+
+	private String[] id;
+
+	public String[] getId() {
+		return id;
+	}
+
+	public void setId(String[] id) {
+		this.id = id;
+	}
+
+}
