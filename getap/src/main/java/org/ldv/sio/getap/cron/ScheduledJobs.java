5eb068b72694fe64e6415907f2fbf459b098a10c
Ivalys <jerome.pros@gmail.com>
2014-06-30 12:02:23 +0200

    Ajout de la librairie mail.jar + mail.properties, et découpe dans le web-application

--- getap/src/main/java/org/ldv/sio/getap/cron/ScheduledJobs.java
+++ getap/src/main/java/org/ldv/sio/getap/cron/ScheduledJobs.java
@@ -67,7 +67,6 @@ public class ScheduledJobs {
 	 * secondes, etc..
 	 */
 
-	private String destinataireMail;
 	private String subjectMail;
 	private String contentMail = "";
 	private String nouvellesDemandes = "";
@@ -89,6 +88,7 @@ public class ScheduledJobs {
 					.getCurrentAnneeScolaire());
 
 			if (dateToday == GregorianCalendar.MONDAY) {
+
 				DateFormat dateFormatEnvoi = new SimpleDateFormat("dd/MM/yyyy");
 				dateFormatEnvoi.setTimeZone(TimeZone
 						.getTimeZone("Europe/Paris"));
